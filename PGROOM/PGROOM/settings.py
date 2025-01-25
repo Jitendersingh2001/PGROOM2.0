bf55855d0595dc976c782b8b2a018b18b9a8a813
jitender singh <jitendersinghjatt09@gmail.com>
2025-01-26 00:49:54 +0530

    allowed all host

--- PGROOM/PGROOM/settings.py
+++ PGROOM/PGROOM/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '^x7l1@j130ffqporbqcf#c$$k3q5#2tq#^6esueu%kjq)c=!iv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
