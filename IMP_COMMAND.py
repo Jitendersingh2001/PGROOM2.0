74a06d3341f5fb3e9bc4b994b9fda0009b386cca
jitender singh <jitendersinghjatt09@gmail.com>
2025-01-25 22:40:51 +0530

    init the project

---
+++ IMP_COMMAND.py
@@ -0,0 +1,7 @@
+"""
+
+-> TO activate the venv: - source myenv/bin/activate
+-> To Run the python server: - python manage.py runserver
+-> To Run the python server on a specific port: - python manage.py createsuperuser
+
+"""
