8c0c6f07bcaf5c27eaea2cb5b4fbbc8c10218585
jitender singh <jitendersinghjatt09@gmail.com>
2025-01-25 23:32:26 +0530

    did something

--- IMP_COMMAND.py
+++ IMP_COMMAND.py
@@ -4,4 +4,9 @@
 -> To Run the python server: - python manage.py runserver
 -> To Run the python server on a specific port: - python manage.py createsuperuser
 
+-> DOCKER COMMANDS: - 
+docker run --name my_postgres -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin123 -e POSTGRES_DB=pgrooms -p 5432:5432 -d postgres
+
+-> TO cerate an app: - python manage.py startapp <app_name>
+
 """
