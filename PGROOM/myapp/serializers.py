f69b5a8429544bedc211a7686e7837d682be3807
jitender singh <jitendersinghjatt09@gmail.com>
2025-01-27 23:17:44 +0530

    make the validator module for data validation

--- PGROOM/myapp/serializers.py
+++ PGROOM/myapp/serializers.py
@@ -1,14 +1,15 @@
-from django.contrib.auth.models import State
 from rest_framework import serializers
+from django.contrib.auth.models import State
+from myapp.validator import validateState
 
 class StateSerializer(serializers.ModelSerializer):
     class Meta:
         model = State
         fields = ["id"]
 
+    def validate(self, data):
+        validateState(data)
+        return data
+
     def find(self, validated_data):
-        # Check if 'id' is present in validated data
-        if 'id' not in validated_data:
-            raise serializers.ValidationError({"id": "This field is required."})
-        # If 'id' is present, retrieve the State object
         return State.objects.get(id=validated_data["id"])
---
+++ PGROOM/myapp/validator/__init__.py
@@ -0,0 +1,1 @@
+from .stateValidator import validateState
