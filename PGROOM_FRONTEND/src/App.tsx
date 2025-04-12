a2067c889a8da63afc672cb48814889fd20b270b
jitender singh <jitendersinghjatt09@gmail.com>
2025-04-12 16:28:40 +0530

    initialize React app with TypeScript, Vite, and Tailwind CSS setup

---
+++ PGROOM_FRONTEND/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-700">Welcome to My App!</h1>
+    </div>
+  )
+}
+
+export default App
